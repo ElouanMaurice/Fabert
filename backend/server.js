8cdbebd5bfd483f4d45fdd8410d1a2566894250c
ElouanMaurice <elouan.maurice@gmail.com>
2025-08-15 08:44:51 +0200

    jhhd

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
-  'https://admin-fabert.vercel.app',
+  'https://fabert.vercel.app',
   'https://agence-fabert.vercel.app'
 ];
 
